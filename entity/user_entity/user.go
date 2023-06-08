0609840652e02adb769b41b99c59e34c00333012
im.anggasaputra <im.anggasaputra@gmail.com>
2023-06-08 20:39:14 +0700

    group user entity

--- entity/user_entity/user.go
+++ entity/user_entity/user.go
@@ -22,10 +22,23 @@ const (
 type UserRepository interface {
 	FindUserByUsername(username string) (UserModel, error)
 	FindUserById(id int64) (UserModel, error)
+	FindUserByIds(ids []int64) ([]UserModel, error)
 	FindUser(gender string, excldeUserIds []int64) (UserModel, error)
 	CreateNewUser(*UserModel) error
 }
 
 type UserUseCase interface {
 	GetRandomUserProfile(ctx context.Context, input DetailUserInput) (DetailUserOutput, error)
+}
+
+const (
+	DAILY_USER_VIEW_PROFILES string = "daily_user_view_profiles:"
+	DAILY_USER_SWAP_PROFILES string = "daily_user_swap_profiles:"
+)
+
+type UserCacheRepository interface {
+	SetDailyUserViewProfile(ctx context.Context, userID, viewedUserId int64) error
+	GetDailyUserViewProfile(ctx context.Context, userID int64) ([]int64, error)
+	SetDailyUserSwapProfile(ctx context.Context, userID, swapedUserId int64) error
+	GetDailyUserSwapProfile(ctx context.Context, userID int64) ([]int64, error)
 }
